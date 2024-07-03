8f02ada54e6ecf798a64cadd245696064a902b5c
guptarakesh198 <guptarakesh198@gmail.com>
2024-07-03 10:40:33 +0530

    try to fix with different approch

--- src/Avatar.php
+++ src/Avatar.php
@@ -91,16 +91,10 @@ class Avatar
         $this->defaultTheme = $this->validateConfig($config);
         $this->applyTheme($this->defaultTheme);
 
-        //get initial themes list from config
-        $this->themes = (isset($config['themes'])  && is_array($config['themes'])) ? $config['themes'] : config('laravolt.avatar.themes', []);
-
-        //applying default theme
-        if ($this->theme == null) {
-            $this->theme = config('laravolt.avatar.theme', null);
-        }
+        $config['themes'] = (isset($config['themes'])  && is_array($config['themes'])) ? $config['themes'] : config('laravolt.avatar.themes', []);
 
         // Add any additional themes for further use
-        $themes = $this->resolveTheme('*', $this->themes ?? []);
+        $themes = $this->resolveTheme('*', $config['themes'] ?? []);
         foreach ($themes as $name => $config) {
             $this->addTheme($name, $config);
         }
